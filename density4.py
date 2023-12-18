df0b5cd8e4819620d98965afe310e48007957d87
davidarenasr <davidarenasr@gmail.com>
2023-12-18 18:44:47 -0500

    two exercise  agregations

---
+++ density4.py
@@ -0,0 +1,13 @@
+#To determine the density of a rectangular metal bar, a student made the following measurements: 
+# length, 8.53 cm; width, 2.4 cm; height, 1.0 cm; mass, 52.7064 g. 
+# Calculate the density of the metal with the correct number of significant figures.
+
+length = 8.53 / 100
+width = 2.4 / 100 
+heigth = 1 / 100
+mass = 52.7064
+volume = length * width * heigth
+density = mass / volume
+
+print ("the density of a metal bar isTo determine the density of a rectangular metal bar, a student made the following measurements: length, 8.53 cm; width, 2.4 cm; height, 1.0 cm; mass, 52.7064 g. Calculate the density of the metal with the correct number of significant figures. " + str(density) + " gr/m**3" )
+
---
+++ density5.py
@@ -0,0 +1,11 @@
+# A piece of metallic silver (Ag) weighing 194.3 g is placed in a graduated cylinder containing 
+# 242 ml of water. The reading in the graduated cylinder is now 260.5 ml. 
+# Calculate the density of silver with this data.
+
+weigth = 193.3 
+graduate_cylinder = 242 
+cylinder_piece_silver = 260.5
+volume_piece = cylinder_piece_silver - graduate_cylinder
+density = weigth / volume_piece
+
+print ("the density is " + str(density) + "gr/ml")
